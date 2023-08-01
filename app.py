41edb672641294f86cbc903e32837e3cd650e9b5
nathanearth616 <wrkclf@gmail.com>
2023-07-31 15:09:42 -1200

    add the code for moving pictures

--- app.py
+++ app.py
@@ -1,15 +1,25 @@
 import os
 import shutil
 
-video_location = 'C:\\Users\\miyamoto\\Downloads\\'
+the_thing_ein_location = 'C:\\Users\\miyamoto\\Downloads\\'
 location = 'C:\\Users\\miyamoto\\Videos\\yt_videos\\'
+pic_location = 'C:\\Users\\miyamoto\\Pictures\\'
 
-videos = [f for f in os.listdir(video_location) if '.mp4' in f.lower()]
+
+youtube_videos = [f for f in os.listdir(the_thing_ein_location) if '.mp4' in f.lower()]
+pictures = [f for f in os.listdir(the_thing_ein_location) if '.png' in f.lower() or '.jpg' in f.lower() or '.jpeg' in f.lower()]
 
 
-for video in videos:
-    where_from = os.path.join(video_location, video)
-    where_to = os.path.join(location, video)
-    shutil.move(where_from, where_to)
+for video in youtube_videos:
+    where_video_from = os.path.join(the_thing_ein_location, video)
+    where_video_to = os.path.join(location, video)
+    shutil.move(where_video_from, where_video_to)
 
-print('files moved successfullu')
+for picture in pictures:
+    where_pic_from = os.path.join(the_thing_ein_location, picture)
+    where_pic_to = os.path.join(pic_location, picture)
+    shutil.move(where_pic_from, where_pic_to)
+    print('picture(s) relocated')
+
+
+print('files moved successfully')
