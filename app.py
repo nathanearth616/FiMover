102c8d5fd53250db067584ba07b2bc3e673dd6b5
nathanearth616 <wrkclf@gmail.com>
2023-08-06 13:45:00 -1200

    add support for coding related vids

--- app.py
+++ app.py
@@ -5,7 +5,8 @@ import shutil
 
 # variabling the locations
 file_location = 'C:\\Users\\miyamoto\\Downloads\\'
-location = 'C:\\Users\\miyamoto\\Videos\\yt_videos\\'
+yt_location = 'C:\\Users\\miyamoto\\Videos\\yt_videos\\'
+cs_location = 'C:\\Users\\miyamoto\\Videos\\Programming\\'
 pic_location = 'C:\\Users\\miyamoto\\Pictures\\'
 
 #checking if movies are still being torrented
@@ -14,16 +15,28 @@ pic_location = 'C:\\Users\\miyamoto\\Pictures\\'
 # if question == 'yes':
 
 # listing the items in the download folder and checking if it contains specitfic words
+cs_videos = [f for f in os.listdir(file_location) if 'python' in f.lower()
+             or 'coding' in f.lower() or 'linux' in f.lower()]
+
 youtube_videos = [f for f in os.listdir(file_location) if '.mp4' in f.lower()]
+
 pictures = [f for f in os.listdir(file_location) if '.png' in f.lower()
             or '.jpg' in f.lower() or '.jpeg' in f.lower()]
+
 # movies = [f for f in os.listdir(file_location) if 'so1' in f.lower()
 #             or '720p' in f.lower() or 'bluray' in f.lower() or 'complete' in f.lower()]
 
 # for loops that moves the file that has that specific word from the download folder to where i want
+for item in cs_videos:
+    where_video_from = os.path.join(file_location, item)
+    where_video_to = os.path.join(cs_location, item)
+    shutil.move(where_video_from, where_video_to)
+
+    print(f'{item} relocated to {where_video_to}')
+
 for video in youtube_videos:
     where_video_from = os.path.join(file_location, video)
-    where_video_to = os.path.join(location, video)
+    where_video_to = os.path.join(yt_location, video)
     shutil.move(where_video_from, where_video_to)
 
     print(f'{video} relocated to {where_video_to}')
